ec78161b9c5695090b897d98e3cd60efbea343e4
Caleb Cross <calebcross@live.com>
2024-10-18 15:11:35 +0200

    Change projects to recent work

--- src/components/section-projects/index.jsx
+++ src/components/section-projects/index.jsx
@@ -7,7 +7,7 @@ const SectionProjects = ({ projects }) => {
   if (!projects.length) return null;
 
   return (
-    <Section title="Projects">
+    <Section title="Recent Work">
       {projects.map((project) => (
         <SummaryItem
           key={project.name}
